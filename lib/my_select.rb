1f7a5bfd00c84c249ead0bda7ab5aec08f255892
Nate Hunter <hunter.j.nate@gmail.com>
2018-12-27 04:10:56 +0000

    Automatically backed up by Learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,8 @@
+-- A few things to think about:
+
+What is the return value of yielding a character to a block?
+How can you ensure your .select method can respond appropriately to being passed an argument of an empty collection?
+
 def my_select(collection)
  # your code here!
 end
